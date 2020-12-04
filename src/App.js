c01106e990b78b62ddb2b390ccfa2e4f0e5aa4af
RyanSamman <RyanSamman@outlook.com>
2020-12-04 22:56:45 +0300

    Data Processing, JSON Files, and Cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import { Route, Switch } from 'react-router';
+// 
 import { useEffect, useState } from 'react';
+
+import { Route, Switch } from 'react-router';
+
+// Components
+import MarkdownFile from './components/MarkdownFile';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
+// Pages
 import Home from './pages/Home';
 import CourseCard from './pages/CourseCard';
 import GradeCard from './pages/GradeCard';
@@ -9,7 +16,12 @@ import RubricCards from './pages/RubricCards';
 import Writing from './pages/Writing';
 import Values from './pages/Values';
 import Buy from './pages/Buy';
+import Page404 from './pages/Page404';
+import TextToSpeech from './pages/TextToSpeech';
+import OurProposal from './pages/OurProposal';
 
+// Markdown
+import StoryMarkdown from './markdown/Story.md'
 
 function App() {
   const [width, setWidth] = useState(window.innerWidth);
@@ -21,15 +33,18 @@ function App() {
     return () => window.removeEventListener('resize', resize);
   });
 
-  const cardWidth = 0.8 * width < 800 ? 0.8 * width : "700px";
+  const cardWidth = 0.8 * width < 800 ? 0.8 * width : 800;
 
   return (
     <>
       <Navbar />
-
       <Switch>
         <Route exact path="/">
           <Home width={cardWidth} />
+        </Route>
+
+        <Route path="/proposal/ours">
+          <OurProposal width={cardWidth} />
         </Route>
 
         <Route path="/proposal/chosen">
@@ -52,9 +67,22 @@ function App() {
           <Buy width={cardWidth} />
         </Route>
 
+        <Route path="/writing/story">
+          <MarkdownFile width={cardWidth} url={StoryMarkdown} />
+        </Route>
+
         <Route path="/writing">
           <Writing width={cardWidth} />
         </Route>
+
+        <Route path="/tts">
+          <TextToSpeech width={cardWidth} />
+        </Route>
+
+        <Route>
+          <Page404 />
+        </Route>
+
       </Switch>
 
       <Footer />
