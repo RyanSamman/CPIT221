3079573104f99d93d7efbb6f6b7901a6249a5e79
RyanSamman <RyanSamman@outlook.com>
2020-12-15 03:59:49 +0300

    Weekly Writing Descriptions

--- src/writing/writings.js
+++ src/writing/writings.js
@@ -21,64 +21,64 @@ import expressYourself1 from './ExpressYourself1.md';
 
 const writings = {
 	"value": {
-		"name": "My Value",
+		"name": "📈 My Value",
 		"path": value,
 		"date": new Date(2020, 11, 18),
-		"description": "My Value to Others"
+		"description": "The Value I can provide to others!"
 	},
 	"story": {
-		"name": "My Inspiration",
+		"name": "💡 My Inspiration",
 		"path": story,
 		"date": new Date(2020, 11, 13),
-		"description": "I talk about my sources of inspiration and growth, and I hope you find my experience ",
+		"description": "The Sources of Inspiration I've had over the years!",
 	},
 	"strategy": {
-		"name": "Teaching Strategy",
+		"name": "👨‍🏫 Teaching Strategy",
 		"path": strategy,
 		"date": new Date(2020, 11, 7),
-		"description": "Evaluating CPIT221's Teaching Strategy"
+		"description": "Evaluating CPIT221's Teaching Strategy."
 	},
 	"robot": {
-		"name": "Am I a Robot?!",
+		"name": "🤖 Am I a Robot?!",
 		"path": robot,
 		"date": new Date(2020, 10, 31),
-		"description": "We just finished our Midterms, and I was burnt out!"
+		"description": "Midterm Burnout!"
 	},
 	"networking": {
-		"name": "Networking & Forming a Team",
+		"name": "🤼 Networking & Forming a Team",
 		"path": networking,
 		"date": new Date(2020, 10, 19),
-		"description": "How I will form my team!"
+		"description": "How I will form my team for the proposal!"
 	},
 	"teamwork": {
-		"name": "Teamwork",
+		"name": "💼 Teamwork",
 		"path": teamwork,
 		"date": new Date(2020, 10, 13),
-		"description": "Reflecting on my Teamwork Skills"
+		"description": "Why are we 'bad' at Teamwork Skills?"
 	},
 	"visual-design": {
-		"name": "Why Visual Design is Important",
+		"name": "🎇 Why Visual Design is Important",
 		"path": visualDesign,
 		"date": new Date(2020, 9, 2),
-		"description": "Analyzing CPIT221's Slides and Design"
+		"description": "Analyzing CPIT221's Slides and Design."
 	},
 	"online-conversation": {
-		"name": "Analysis of the Group Meeting",
+		"name": "⚖ Analysis of the Online Conversation",
 		"path": onlineConversation,
 		"date": new Date(2020, 9, 28),
-		"description": ""
+		"description": "Reflecting on my role in a Conversation with Strangers."
 	},
 	"express-yourself2": {
-		"name": "Express Yourself - Draft 2",
+		"name": "💻 Express Yourself - Draft 2",
 		"path": expressYourself2,
 		"date": new Date(2020, 9, 25),
-		"description": "Second Draft of Express Yourself"
+		"description": "Second Draft of Express Yourself."
 	},
 	"express-yourself": {
-		"name": "Express Yourself - Draft 2",
+		"name": "💻 Express Yourself - Draft 1",
 		"path": expressYourself1,
 		"date": new Date(2020, 9, 25),
-		"description": "First Draft of Express Yourself"
+		"description": "First Draft of Express Yourself."
 	},
 }
 
