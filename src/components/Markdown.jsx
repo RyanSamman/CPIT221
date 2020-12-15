e95859b54f257b0efe8173e0e2aad944ecd9e8c8
RyanSamman <RyanSamman@outlook.com>
2020-12-15 04:02:52 +0300

    Formatting

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -7,18 +7,18 @@ import gfm from 'remark-gfm';
 // https://github.com/remarkjs/react-markdown/issues/69
 function flatten(text, child) {
 	return typeof child === 'string'
-	  ? text + child
-	  : React.Children.toArray(child.props.children).reduce(flatten, text)
-  }
-  
-  function HeadingRenderer(props) {
+		? text + child
+		: React.Children.toArray(child.props.children).reduce(flatten, text)
+}
+
+function HeadingRenderer(props) {
 	var children = React.Children.toArray(props.children)
 	var text = children.reduce(flatten, '')
 	var slug = text.toLowerCase().replace(/\W/g, '-')
-	return React.createElement('h' + props.level, {id: slug}, props.children)
-  }  
+	return React.createElement('h' + props.level, { id: slug }, props.children)
+}
 
-const Markdown = ({ width, md, style }) => (	
+const Markdown = ({ width, md, style }) => (
 	<Card border="primary" style={{ width, border: "none", ...style }} className="mx-auto" >
 		<Card.Body className="markdown-body">
 			<ReactMarkdown plugins={[gfm]} renderers={{ heading: HeadingRenderer }} children={md} allowDangerousHtml />
