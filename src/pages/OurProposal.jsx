d5a787ec265169e1cff2b7bb89858307ce3b7232
RyanSamman <RyanSamman@outlook.com>
2020-12-15 17:52:23 +0300

    Embed Tweet

--- src/pages/OurProposal.jsx
+++ src/pages/OurProposal.jsx
@@ -1,12 +1,16 @@
 import MarkdownFile from './../components/MarkdownFile';
 import YoutubeVideo from './../components/YoutubeVideo';
+import { TwitterTweetEmbed } from 'react-twitter-embed';
 import ourProposalMarkdown from './../markdown/OurProposal.md';
+import Skeleton from 'react-loading-skeleton';
 
 const OurProposal = ({ width }) => (
 	<>
 		<MarkdownFile width={width} url={ourProposalMarkdown} />
 		<center>
 			<YoutubeVideo width={width} id={"1lalKs9CZtw"} />
+
+			<TwitterTweetEmbed tweetId="1328294654371131392" placeholder={<Skeleton height={500} />} />
 		</center>
 	</>
 )
