c8c12ca3d4a7edadc0a194ff39d4f33a783894ab
RyanSamman <RyanSamman@outlook.com>
2020-12-15 13:04:48 +0300

    Add markdown to our proposal page

--- src/markdown/OurProposal.md
+++ src/markdown/OurProposal.md
@@ -3,3 +3,5 @@
 [We formed our team as I described in my weekly writing on Networking & Teams!](/writing/networking) Due to plans in the making since I've entered King Abdulaziz University, I had a large amount of people who I was interested in working with. 
 
 However, I decided I wanted to try something new, and I decided to team up with people I've recently met in FCIT. While I don't like being in a leadership position, I was forced to, due to my ability to stay organized.
+
+[Google Drive Link to the Survey Data, Slides, and Full Proposal](https://drive.google.com/drive/folders/1fS-peh5vJn7GGoro9QvfD5v4QFuUaZeo)
--- src/pages/OurProposal.jsx
+++ src/pages/OurProposal.jsx
@@ -1,17 +1,10 @@
-import Markdown from './../components/Markdown';
+import MarkdownFile from './../components/MarkdownFile';
 import YoutubeVideo from './../components/YoutubeVideo';
-
-const title = `
-<center>
-<h1>Our Proposal - Page Not Completed</h1>
-</center>
-
-[Google Drive Link to the Survey Data, Slides, and Full Proposal](https://drive.google.com/drive/folders/1fS-peh5vJn7GGoro9QvfD5v4QFuUaZeo)
-`
+import ourProposalMarkdown from './../markdown/OurProposal.md';
 
 const OurProposal = ({ width }) => (
 	<>
-		<Markdown width={width} md={title} />
+		<MarkdownFile width={width} url={ourProposalMarkdown} />
 		<center>
 			<YoutubeVideo width={width} id={"1lalKs9CZtw"} />
 		</center>
