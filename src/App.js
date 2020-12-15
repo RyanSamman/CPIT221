a80c11b50c4cb43572dd5fdc63524b161a219294
RyanSamman <RyanSamman@outlook.com>
2020-12-15 04:00:47 +0300

    Add Padding

--- src/App.js
+++ src/App.js
@@ -28,61 +28,63 @@ function App() {
 
     return () => window.removeEventListener('resize', resize);
   });
-
-  const cardWidth = 0.8 * width < 800 ? 0.8 * width : 800;
+  const MAX_SIZE = 800;
+  const cardWidth = 0.8 * width < MAX_SIZE ? 0.8 * width : MAX_SIZE;
+  const padding = 0.8 * width < MAX_SIZE ? "10%" : (width - MAX_SIZE) / 2;
 
   return (
     <>
       <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home width={cardWidth} />
-        </Route>
+      <div style={{ paddingLeft: padding, paddingRight: padding }}>
+        <Switch>
+          <Route exact path="/">
+            <Home width={cardWidth} />
+          </Route>
+          
+          <Route path="/proposal/ours">
+            <OurProposal width={cardWidth} />
+          </Route>
 
-        <Route path="/proposal/ours">
-          <OurProposal width={cardWidth} />
-        </Route>
+          <Route path="/proposal/chosen">
+            <CourseCard cardWidth={cardWidth} />
+          </Route>
 
-        <Route path="/proposal/chosen">
-          <CourseCard cardWidth={cardWidth} />
-        </Route>
+          <Route path="/proposal/grades">
+            <GradeCard cardWidth={cardWidth} />
+          </Route>
 
-        <Route path="/proposal/grades">
-          <GradeCard cardWidth={cardWidth} />
-        </Route>
+          <Route path="/proposal/rubric">
+            <RubricCards cardWidth={cardWidth} />
+          </Route>
 
-        <Route path="/proposal/rubric">
-          <RubricCards cardWidth={cardWidth} />
-        </Route>
+          <Route path="/values">
+            <Values width={cardWidth} />
+          </Route>
 
-        <Route path="/values">
-          <Values width={cardWidth} />
-        </Route>
+          <Route path="/buy">
+            <Buy width={cardWidth} />
+          </Route>
 
-        <Route path="/buy">
-          <Buy width={cardWidth} />
-        </Route>
+          <Route
+            exact
+            path="/writing/:id"
+            render={props => <Writing width={cardWidth} id={props.match.params.id} />}>
+          </Route>
 
-        <Route 
-          exact
-          path="/writing/:id"
-          render={props => <Writing width={cardWidth} id={props.match.params.id} />}>
-        </Route>
+          <Route path="/writing">
+            <WeeklyWriting width={cardWidth} />
+          </Route>
 
-        <Route path="/writing">
-          <WeeklyWriting width={cardWidth}/>
-        </Route>
+          <Route path="/tts">
+            <TextToSpeech width={cardWidth} />
+          </Route>
 
-        <Route path="/tts">
-          <TextToSpeech width={cardWidth} />
-        </Route>
+          <Route>
+            <Page404 />
+          </Route>
 
-        <Route>
-          <Page404 />
-        </Route>
-
-      </Switch>
-
+        </Switch>
+      </div>
       <Footer />
     </>
   );
