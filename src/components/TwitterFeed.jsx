48f71914e3a2ef4bf97954e31ef065ba9553953b
RyanSamman <RyanSamman@outlook.com>
2020-12-17 23:02:52 +0300

    Linter

--- src/components/TwitterFeed.jsx
+++ src/components/TwitterFeed.jsx
@@ -1,7 +1,7 @@
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 const TwitterFeed = ({ width }) => (
-	<div style={{display: "flex", justifyContent: "center", alignContent: "center"}}>
+	<div style={{ display: "flex", justifyContent: "center", alignContent: "center" }}>
 		<TwitterTimelineEmbed
 			sourceType="profile"
 			screenName="Ryan_Samman_"
